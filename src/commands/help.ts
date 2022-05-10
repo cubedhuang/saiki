596af6766973e3de23a753f9bf093c7d9ddea45c
cubedhuang <99danielh@gmail.com>
2022-05-09 22:35:39 -0400

    feat: add wordle to help

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -9,7 +9,7 @@ ${Lib.pickRandom(Lib.yareYare)} I suppose I'll explain myself${
 		again ? " to you again." : ""
 	}.
 saiki coffee jelly : pictures
-You can ask me anything, but don't count on an answer.
+You can ask me anything, but don't count on an answer. Also if you send a valid Wordle guess, I'll tell you :).
 `.trim();
 }
 

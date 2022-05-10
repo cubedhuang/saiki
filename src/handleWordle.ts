495afd091cdbf27c6f82e7d98fbb07f4bdf5e3cf
cubedhuang <99danielh@gmail.com>
2022-05-09 22:40:57 -0400

    fix: properly handle uppercase

--- src/handleWordle.ts
+++ src/handleWordle.ts
@@ -3,19 +3,16 @@ import type { Message } from "discord.js";
 import wordle from "../data/wordle.json" assert { type: "json" };
 
 export async function handleWordle(message: Message) {
-	if (!message.content.match(/^[a-zA-Z]{5}$/i)) return false;
+	if (!message.content.match(/^[a-zA-Z]{5}$/)) return false;
 
-	if (
-		!wordle.answers.includes(message.content) &&
-		!wordle.dictionary.includes(message.content)
-	) {
+	const word = message.content.toLowerCase();
+
+	if (!wordle.answers.includes(word) && !wordle.dictionary.includes(word)) {
 		return false;
 	}
 
-	const word =
-		message.content[0].toUpperCase() +
-		message.content.slice(1).toLowerCase();
-	await message.reply(`${word} is a valid Wordle guess.`);
+	const capitalized = word[0].toUpperCase() + word.slice(1);
+	await message.reply(`${capitalized} is a valid Wordle guess.`);
 
 	return true;
 }

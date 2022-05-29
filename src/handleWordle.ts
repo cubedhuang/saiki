bf3b942190d41b9e06622cf169b45361b01fbf0c
cubedhuang <99danielh@gmail.com>
2022-05-29 01:38:30 -0400

    refactor: make trie children optional

--- src/handleWordle.ts
+++ src/handleWordle.ts
@@ -3,17 +3,18 @@ import type { Message } from "discord.js";
 import { wordle } from "../data/wordle";
 
 class TrieNode {
-	children: Record<string, TrieNode> = {};
+	children?: Record<string, TrieNode>;
 
 	add(word: string) {
 		if (word.length === 0) return;
-		if (!this.children[word[0]]) this.children[word[0]] = new TrieNode();
+		this.children ??= {};
+		this.children[word[0]] ??= new TrieNode();
 		this.children[word[0]].add(word.slice(1));
 	}
 
 	has(word: string): boolean {
 		if (word.length === 0) return true;
-		if (!this.children[word[0]]) return false;
+		if (!this.children?.[word[0]]) return false;
 		return this.children[word[0]].has(word.slice(1));
 	}
 }
